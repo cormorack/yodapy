277523bf3f6efadfc311fbaef95352111c26414c
lsetiawan <landungs@uw.edu>
2018-04-20 15:32:41 -0700

    Fix credentialing of file

--- visualocean/__init__.py
+++ visualocean/__init__.py
@@ -3,6 +3,8 @@ from __future__ import (absolute_import,
                         print_function,
                         unicode_literals)
 
+from visualocean.core import OOIASSET
+
 __author__ = 'Landung Setiawan'
 
 __all__ = ['core', 'utils']
